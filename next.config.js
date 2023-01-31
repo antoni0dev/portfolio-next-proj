f537609d2cea27739c387426af7a30dc6cf0c3d5
Antonio Mansory <antoniosimeonov7@gmail.com>
2023-01-31 20:51:59 +0200

    Add image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["https://portfolio-next-proj-nukhpsail-an7oniors.vercel.app/"],
+  },
+};
