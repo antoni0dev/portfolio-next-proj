6a8ce4dc7a9a2263886ada2500771cc0d2fad9c3
Antonio Mansory <antoniosimeonov7@gmail.com>
2023-02-21 08:51:21 +0200

    Fix wrong text size on small screen

--- components/Project.jsx
+++ components/Project.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Project = ({ projectSrc, projectName, technologyUsed, moreInfo }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-[#07F2C5] rounded-xl hover:bg-gradient-to-r from-[#FFEE80] to-[#07F2C5] group">
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-[#07F2C5] rounded-xl hover:bg-gradient-to-r from-[#451f51] to-[#5fc1a9] group">
       <Image
         width={1000}
         height={1000}
@@ -13,10 +13,14 @@ const Project = ({ projectSrc, projectName, technologyUsed, moreInfo }) => {
         alt="/"
       />
       <div className="hidden text-bg-color text-xl group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] max-w-[150px]">
-        <h3 className="text-2xl tracking-wider text-center">{projectName}</h3>
-        <p className="pb-4 pt-2 text-center">{technologyUsed}</p>
+        <h3 className="text-sm md:text-xl lg:text-2xl tracking-wider text-center">
+          {projectName}
+        </h3>
+        <p className="text-sm md:text-xl lg:text-2xl pb-4 pt-2 text-center">
+          {technologyUsed}
+        </p>
         <Link href={moreInfo}>
-          <p className="text-center py-3 rounded-lg bg-bg-color text-white font-bold text-lg cursor-pointer">
+          <p className="w-[143px] md:w-max text-sm md:text-xl lg:text-2xl text-center p-3 rounded-lg bg-bg-color text-white font-bold cursor-pointer">
             More Info
           </p>
         </Link>
